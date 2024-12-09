afac2e99bbd5e46d47ec18515b69421784043bb2
Tomasz Jagodowski <jagodowski.tomasz@gmail.com>
2024-12-09 23:15:19 +0000

    Day 5 Part 2 complete, Day 6 in progress

--- src/Day5.java
+++ src/Day5.java
@@ -7,12 +7,18 @@ import java.util.stream.Collectors;
 public class Day5 {
     public static void main(String[] args) throws IOException {
         System.out.println("The sum of correct orders middle pages is (sample: 143): "
-                +  new Day5().solution("res/Day5-sample.txt"));
+                +  new Day5().solution("res/Day5-sample.txt", calcRules("res/Day5-sample.txt")));
         System.out.println("The sum of correct orders middle pages is : "
-                +  new Day5().solution("res/Day5.txt"));
+                +  new Day5().solution("res/Day5.txt", calcRules("res/Day5.txt")));
+
+        System.out.println("The sum of sorted incorrect orders middle pages is (sample: 123): "
+                +  new Day5().solutionPart2("res/Day5-sample.txt", calcRules("res/Day5-sample.txt")));
+
+        System.out.println("The sum of sorted incorrect orders middle pages is : "
+                +  new Day5().solutionPart2("res/Day5.txt", calcRules("res/Day5.txt")));
     }
 
-    private long solution(String filename) throws IOException {
+   static private HashMap<Integer, Set<Integer>> calcRules(String filename) throws IOException {
 
         var rules = new HashMap<Integer, Set<Integer>>();
         Files.lines(Path.of(filename))
@@ -27,28 +33,50 @@ public class Day5 {
                     }
                     rules.get(updatePage).add(afterPage);
                 });
-
-        //Debug list
-//        rules.forEach((integer, integers) -> System.out.println(integer + "->" + integers));
-
+        return rules;
+    }
+    private long solution(String filename, HashMap<Integer, Set<Integer>> rules) throws IOException {
         return Files.lines(Path.of(filename))
                 .filter(s -> !s.contains("|"))
-                .filter(s -> s.length() > 1)
+                .filter(s -> s.length() > 1) //adding filter so we don't parse empty line
                 .map(textOrders -> textOrders.split(","))
-                .map(stringArray ->
+                .map(stringArray -> //convert to list of int'ss
                     Arrays.stream(stringArray)
                             .mapToInt(Integer::parseInt)
                             .boxed()
                             .collect(Collectors.toList())
                 )
-                .peek(integers -> System.out.println("LIST: " + integers))
+//                .peek(integers -> System.out.println("LIST: " + integers))
                 .mapToLong(numberList -> isInOrder(numberList, rules))
-                .peek(System.out::println)
+//                .peek(System.out::println)
                 .sum();
     }
 
+    private long solutionPart2(String filename, HashMap<Integer, Set<Integer>> rules) throws IOException {
+        return Files.lines(Path.of(filename))
+                .filter(s -> !s.contains("|"))
+                .filter(s -> s.length() > 1) //adding filter so we don't parse empty line
+                .map(textOrders -> textOrders.split(","))
+                .map(stringArray -> //convert to list of int'ss
+                        Arrays.stream(stringArray)
+                                .mapToInt(Integer::parseInt)
+                                .boxed()
+                                .collect(Collectors.toList())
+                )
+                .filter(numberList -> isInOrder(numberList, rules) == 0) // 0 means not in order
+                .peek(integers -> System.out.println("Unordered LIST: " + integers))
+                .map(integers ->  integers
+                        .stream()
+                        .sorted((o1, o2) -> rules.get(o1) == null || !rules.get(o1).contains(o2) ? 1 : -1)
+                        .toList())
+                .peek(integers -> System.out.println("Ordered LIST: " + integers))
+                .mapToLong(orderedList ->  orderedList.get(orderedList.size() / 2))
+//                .peek(System.out::println)
+                .sum();
+    }
+
+
     long isInOrder(List<Integer> updatePages, HashMap<Integer, Set<Integer>> rules) {
-        List<Integer> pagesCopy = new ArrayList<>(updatePages);
         List<Integer> priorPages = new ArrayList<>();
         long count =
                 updatePages
@@ -61,17 +89,16 @@ public class Day5 {
                     long countValidPages = priorPages
                             .stream()
                             .filter(lookingAtPage -> !Objects.equals(lookingAtPage, currPage))
-                            .peek(value -> System.out.println(
-                                    "checking " + currPage + " is blocked by " + value +
-                                    " comparing with rules: "        + rules.get(currPage).stream().toList()))
+//                            .peek(value -> System.out.println(
+//                                    "checking " + currPage + " is blocked by " + value +
+//                                    " comparing with rules: "        + rules.get(currPage).stream().toList()))
                             .filter(page -> !rules.get(currPage).contains(page))
-                            .peek(page -> System.out.println("Page: " + currPage + " not blocked by " + page + " rules:"
-                            +  rules.get(currPage).stream().toList()))
+//                            .peek(page -> System.out.println("Page: " + currPage + " not blocked by " + page + " rules:"
+//                            +  rules.get(currPage).stream().toList()))
                             .count();
 
-                    pagesCopy.remove(currPage);
                     priorPages.add(currPage);
-                    System.out.println("countValidPages: " + countValidPages + " priorPages size: " + priorPages.size());
+//                    System.out.println("countValidPages: " + countValidPages + " priorPages size: " + priorPages.size());
                     return countValidPages == priorPages.size() -1;
                 })
                 .count();
--- src/Day6.java
+++ src/Day6.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.IntStream;
 
 public class Day6 {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,25 @@ public class Day6 {
     }
 
     private long solution(String filename) throws IOException {
+
+        int gridWidth  = Files.lines(Path.of(filename)).findAny().get().length();
+        int gridHeight = (int) Files.lines(Path.of(filename)).count();
+        enum FIELD_STATUS { EMPTY, VISITED, OBSTRUCTION}
+        FIELD_STATUS[][] grid = new FIELD_STATUS[gridWidth][gridHeight];
+
+//        IntStream
+//                .range(0, gridWidth)
+//                .forEach(x -> IntStream
+//                        .range(0, gridHeight)
+//                        .forEach(y -> {
+//                            Files.lines(Path.of(filename)).
+//                            switch ("dupa") {
+//                                case "dupa":
+//
+//                            }
+//                            grid[x][y] = 0;
+//                        }));
+
         Files.lines(Path.of(filename))
                 .forEach(System.out::println);
         return 0;
